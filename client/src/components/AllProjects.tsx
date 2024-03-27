1849a630820fbe4f79a692d041680e0e47456202
minhtrifit <minhtri.fit@gmail.com>
2024-03-27 16:53:16 +0700

    fix all project mobile view

--- client/src/components/AllProjects.tsx
+++ client/src/components/AllProjects.tsx
@@ -9,6 +9,8 @@ import {
 } from "@/components/ui/pagination";
 import { v4 as uuidv4 } from "uuid";
 
+import useSize from "@/hooks/useSize";
+
 import ProjectCard from "./ProjectCard";
 
 import { ProjectType } from "@/types";
@@ -20,8 +22,11 @@ interface PropType {
 const AllProjects = (props: PropType) => {
   const { projects } = props;
 
-  const ITEMS_PER_PAGE = 9;
+  // const ITEMS_PER_PAGE = 9;
 
+  const windowsize = useSize();
+
+  const [itemsPerPage, setItemsPerPage] = useState<number>(9);
   const [pagination, setPagination] = useState<number[]>([]);
   const [avtivePag, setActivePag] = useState<number>(1);
   const [projectsPerPage, setProjectsPerPage] = useState<ProjectType[]>([]);
@@ -33,14 +38,14 @@ const AllProjects = (props: PropType) => {
     );
 
   const handleCountPages = () => {
-    const pageCount = Math.ceil(projects?.length / ITEMS_PER_PAGE);
+    const pageCount = Math.ceil(projects?.length / itemsPerPage);
     console.log("PAGE COUNT:", pageCount);
     setPagination(arrayRange(1, pageCount, 1));
   };
 
   const handleGetProjectsPerPage = (n: number) => {
-    const begin = (n - 1) * ITEMS_PER_PAGE;
-    const end = (n - 1) * ITEMS_PER_PAGE + ITEMS_PER_PAGE;
+    const begin = (n - 1) * itemsPerPage;
+    const end = (n - 1) * itemsPerPage + itemsPerPage;
 
     const items = projects?.slice(begin, end);
     setProjectsPerPage(items);
@@ -49,19 +54,25 @@ const AllProjects = (props: PropType) => {
   useEffect(() => {
     handleCountPages();
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [itemsPerPage]);
 
   useEffect(() => {
     handleGetProjectsPerPage(avtivePag);
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [avtivePag]);
+  }, [avtivePag, itemsPerPage]);
+
+  useEffect(() => {
+    if (windowsize[0] <= 640) setItemsPerPage(3);
+    else setItemsPerPage(9);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [windowsize]);
 
   return (
     <div className="mt-10">
       <div className="flex flex-wrap gap-3">
         {projectsPerPage?.map((project: ProjectType) => {
           return (
-            <div key={uuidv4()} className="pb-3 w-[200px] lg:w-[300px]">
+            <div key={uuidv4()} className="pb-3 w-[100%] sm:w-[300px]">
               <ProjectCard project={project} />
             </div>
           );
---
+++ client/src/hooks/useSize.ts
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+import { isMobile } from "react-device-detect";
+
+const useSize = () => {
+  const [windowSize, setWindowSize] = useState([0, 0]);
+
+  const windowSizeHandler = () => {
+    setWindowSize([window.innerWidth, window.innerHeight]);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", windowSizeHandler);
+
+    return () => {
+      window.removeEventListener("resize", windowSizeHandler);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (!isMobile) setWindowSize([1879, 618]);
+  }, []);
+
+  return windowSize;
+};
+
+export default useSize;
